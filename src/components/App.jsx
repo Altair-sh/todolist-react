b5c6f259d7311b81223735b9cec87b9533fcfc8d
Timerix <timerix11@gmail.com>
2025-02-04 12:22:36 +0500

    step 11: item id

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,17 @@ import { TodoList } from './TodoList'
 
 export const App = () => {
     const todoData = [
-        { label: 'Drink Coffee', important: false },
-        { label: 'Implement React Application', important: true },
-        { label: 'Make notes from your stud', important: false },
-        { label: 'Do not drink alchohol', important: false },
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Implement React Application', important: true, id: 2 },
+        { label: 'Make notes from your stud', important: false, id: 3 },
+        { label: 'Do not drink alchohol', important: false, id: 4 },
     ];
 
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList todos =  {todoData} />
+            <TodoList todos = {todoData} />
         </div>
     )
 }
