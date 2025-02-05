843e64b2ebf4be0e8bb12c375457b53d3c5b9b65
Timerix <timerix11@gmail.com>
2025-02-05 11:40:02 +0500

    step 16: App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,12 +15,12 @@ export const App = () => {
 
     return (
         <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
+            <AppHeader toDo={1} done={3}/>
             <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+                <SearchPanel/>
+                <ItemStatusFilter/>
             </div>
-            <TodoList todos={todoData} />
+            <TodoList todos={todoData}/>
         </div>
     )
 }
